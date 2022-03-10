ed8b3d47f503743a177e3eceb3fe6214af88a4bd
zouzoulukankanshu <280946274@qq.com>
2022-03-10 15:04:35 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('index.py')
+
+def login(request):
+    return redirect('/index')
